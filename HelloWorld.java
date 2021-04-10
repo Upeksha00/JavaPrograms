57906615a1edc9ff74a8ff5929d03ae9dce6f5da
Upeksha0 <upekshadilshan000@gmail.com>
2021-04-10 11:28:51 +0530

    I added another print line to HelloWorld.java with \n and \t

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello Java");
+		System.out.println("I'm Upeksha \n I know java little bit \t Java is a nice language");
 
 }
 
